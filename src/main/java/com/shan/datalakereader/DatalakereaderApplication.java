bf8d5530e1a15c655b21537ea20ffc33983897ee
Shanmuga Sivapragasam <shanmuga.sivapragasam@gmail.com>
2019-08-31 23:42:47 -0400

    [#0] init commit

---
+++ src/main/java/com/shan/datalakereader/DatalakereaderApplication.java
@@ -0,0 +1,13 @@
+package com.shan.datalakereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalakereaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalakereaderApplication.class, args);
+	}
+
+}
